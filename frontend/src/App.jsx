9090020fa968732c10eb0103ab9ab1c934c4a242
Gauravmahto187 <gauravmahto269@gmail.com>
2024-10-09 15:06:04 +0545

    feat: add routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,8 +1,15 @@
+import {BrowserRouter, Routes, Route, Navigate} from 'react-router-dom'
+import Login from './pages/Login'
+import AdminDashboard from './pages/AdminDashboard'
 function App() {
   return (
-    <div className='text-4xl text-teal-400 text-center'>
-      Welcome to Empora
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Navigate to="/admin-dashboard"/>}></Route>
+      <Route path="/login" element={<Login />}></Route>
+      <Route path="/admin-dashboard" element={<AdminDashboard />}></Route>
+    </Routes>
+    </BrowserRouter>
   )
 }
 
