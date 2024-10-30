e67507c92a65868844ae76420a9f2e80c5d0f04f
Gauravmahto187 <gauravmahto269@gmail.com>
2024-10-30 21:50:18 +0545

    feat: Update employee fucntionality backend

--- server/controllers/employeeController.js
+++ server/controllers/employeeController.js
@@ -88,4 +88,32 @@ const getEmployee = async (req, res) => {
   }
 }
 
-export { addEmployee, upload, getEmployees, getEmployee };
+const updateEmployee = async (req, res) => {
+  try{
+    const {id} = req.params;
+    const {name, maritalStatus, designation, department, salary} = req.body;
+
+    const employee = await Employee.findById({_id: id})
+    if(!employee){
+      return res.status(404).json({success: false, error: "Employee not found"})
+    }
+
+    const user = await User.findById({_id: employee.userId})
+    if(!user){
+      return res.status(404).json({success: false, error: "User not found"})
+    }
+
+    const updateUser = await User.findByIdAndUpdate({_id: employee.userId}, {name})
+    const updateEmployee = await Employee.findByIdAndUpdate({_id: id}, {maritalStatus, designation, department, salary })
+
+    if(!updateEmployee || !updateUser) {
+      return res.status(404).json({success: false, error: "Document not found"})
+    }
+
+    return res.status(200).json({success: true, message: "Employee updated"})
+  } catch(error) {
+    return res.status(500).json({success: false, error: "Update employees server error"})
+  }
+}
+
+export { addEmployee, upload, getEmployees, getEmployee, updateEmployee };
