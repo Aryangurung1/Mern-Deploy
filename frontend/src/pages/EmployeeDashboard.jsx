31dffda4df442216e6fe541cf6b192936c76b8b5
Gauravmahto187 <gauravmahto269@gmail.com>
2024-11-01 21:48:17 +0545

    feat: Employee Dashboard

--- frontend/src/pages/EmployeeDashboard.jsx
+++ frontend/src/pages/EmployeeDashboard.jsx
@@ -1,9 +1,16 @@
+import { Outlet } from "react-router-dom"
+import Navbar from "../components/Dashboard/Navbar"
+import Sidebar from "../components/Dashboard/EmployeeDashboard/Sidebar"
 
 
 const EmployeeDashboard = () => {
   return (
-    <div>
-      <div>Employee Dashboard</div>
+    <div className="flex">
+      <Sidebar />
+      <div className="flex-1 ml-64 bg-gray-100 h-screen">
+        <Navbar />
+        <Outlet />
+      </div>
     </div>
   )
 }
