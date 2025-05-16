b2790953f5ab0c1de033bbcd280d268988fb3709
Gauravmahto187 <gauravmahto269@gmail.com>
2025-05-16 18:24:37 +0545

    connect db

--- server/db/db.js
+++ server/db/db.js
@@ -2,9 +2,9 @@ import mongoose from 'mongoose'
 
 const connectToDatabase = async () => {
     try {
-        const MONGODB_URI = process.env.MONGODB_URI || 'mongodb://localhost:27017/Empora';
+        const MONGODB_URL = process.env.MONGODB_URL || 'mongodb://localhost:27017/Empora';
         console.log('Connecting to MongoDB...');
-        await mongoose.connect(MONGODB_URI);
+        await mongoose.connect(MONGODB_URL);
         console.log('Connected to MongoDB successfully');
     } catch(error){
         console.error('MongoDB connection error:', error);
